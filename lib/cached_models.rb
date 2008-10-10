919351e8fa5760d28504a3706695a2050b519ba0
Luca Guidi <guidi.luca@gmail.com>
2008-10-10 12:12:54 +0200

    Created separated folder for ActiveRecord

--- lib/cached_models.rb
+++ lib/cached_models.rb
@@ -1,4 +1,4 @@
 require 'activesupport' unless defined? Rails
-require File.dirname(__FILE__) + '/active_record'
+require File.dirname(__FILE__) + '/activerecord/lib/active_record'
 
 ActiveRecord::Base.rails_cache = Rails.cache if defined? Rails
